6bfa1f3790f79bf2dab1c8e986052f25ab9dcc2d
Mosi sebastião <mosijoao4002@gmail.com>
2024-10-18 17:33:13 -0300

    Add files via upload

---
+++ styl/doc.js
@@ -0,0 +1,11 @@
+function mercado(){
+    window.document.location.href="mercado_livre.html"
+}
+
+function spotify(){
+    window.document.location.href="spotify-pg1.html"
+}
+
+function crud(){
+    window.document.location.href="doc_pg_1.html"
+}
